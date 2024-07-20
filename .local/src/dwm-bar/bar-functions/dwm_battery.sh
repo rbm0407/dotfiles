5dd0e85ca92ebb060162eed14a12ef82a9cef95c
rbm0407 <rodrigo@rbm.dev.br>
2024-07-20 20:12:12 +0000

    improve dwm-bar battery section to allow multiple options

--- .local/src/dwm-bar/bar-functions/dwm_battery.sh
+++ .local/src/dwm-bar/bar-functions/dwm_battery.sh
@@ -6,8 +6,8 @@
 
 dwm_battery () {
     # Change BAT1 to whatever your battery is identified as. Typically BAT0 or BAT1
-    CHARGE=$(cat /sys/class/power_supply/BAT0/capacity)
-    STATUS=$(cat /sys/class/power_supply/BAT0/status)
+    CHARGE=$(cat /sys/class/power_supply/BAT$1/capacity)
+    STATUS=$(cat /sys/class/power_supply/BAT$1/status)
 
     printf "%s" "$SEP1"
     if [ "$IDENTIFIER" = "unicode" ]; then
@@ -21,6 +21,3 @@ dwm_battery () {
     fi
     printf "%s\n" "$SEP2"
 }
-
-dwm_battery
-
