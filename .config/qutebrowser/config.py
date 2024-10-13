3d9b59fb02ca30b205393ef798bbdcb08ca91e68
Rodrigo Borges <rodrigo.borges.de.moraes@gmail.com>
2024-10-13 17:32:59 -0300

    use rio as editor, set start page to about:blank and search page to search.brave.com

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,3 +1,10 @@
 config.load_autoconfig()
-config.set('editor.command', ['wezterm', '-e', 'nvim', '{file}'])
+config.set('editor.command', ['rio', '-e', 'nvim', '{file}'])
 #config.set('colors.webpage.darkmode.enabled', True)
+
+config.set('url.start_pages', ['about:blank'])
+
+config.set(
+    'url.searchengines',
+    {'DEFAULT': 'https://search.brave.com/search?q={}'}
+)
