18c49e9081491a87e520d61b5ddef48543d675aa
Rodrigo Borges <rodrigo.borges.de.moraes@gmail.com>
2024-10-13 17:41:28 -0300

    add Hyprland (wayland) configs and change default term to rio

--- .local/src/dwm-6.5/config.h
+++ .local/src/dwm-6.5/config.h
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 /*static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };*/
 static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL };
-static const char *termcmd[]  = { "wezterm", "start", "--", "tmux", NULL };
+static const char *termcmd[]  = { "rio", "-e", "tmux", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
