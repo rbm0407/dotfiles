2f883a0177233f3940da80da755454174c03ae65
Rodrigo Borges <rodrigo@rbm.dev.br>
2024-11-20 17:16:01 +0000

    split the qutebrowser personal settings to another file

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,10 +1,5 @@
 config.load_autoconfig()
 config.set('editor.command', ['rio', '-e', 'nvim', '{file}'])
-#config.set('colors.webpage.darkmode.enabled', True)
+# config.set('colors.webpage.darkmode.enabled', True)
 
-config.set('url.start_pages', ['about:blank'])
-
-config.set(
-    'url.searchengines',
-    {'DEFAULT': 'https://search.brave.com/search?q={}'}
-)
+config.source('privacy.py')
