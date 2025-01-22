9dbb6bc2dae9923d4ec1c58f74a96c1e9a0e9546
Rodrigo Borges <rodrigo@rbm.dev.br>
2025-01-22 17:42:52 +0000

    adjust yamlls plugin to parse kubernetes and another devops tools schemas

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -670,6 +670,22 @@ require('lazy').setup({
             },
           },
         },
+        yamlls = {
+          settings = {
+            yaml = {
+              schemas = {
+                kubernetes = '0*.yml',
+                ['http://json.schemastore.org/github-workflow'] = '.github/workflows/*',
+                ['http://json.schemastore.org/github-action'] = '.github/action.{yml,yaml}',
+                ['http://json.schemastore.org/ansible-stable-2.9'] = 'roles/tasks/**/*.{yml,yaml}',
+                ['http://json.schemastore.org/prettierrc'] = '.prettierrc.{yml,yaml}',
+                ['http://json.schemastore.org/kustomization'] = 'kustomization.{yml,yaml}',
+                ['http://json.schemastore.org/chart'] = 'Chart.{yml,yaml}',
+                ['http://json.schemastore.org/circleciconfig'] = '.circleci/**/*.{yml,yaml}',
+              },
+            },
+          },
+        },
       }
 
       -- Ensure the servers and tools above are installed
