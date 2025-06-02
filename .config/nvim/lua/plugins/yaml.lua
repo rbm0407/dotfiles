075bf8620241947fc2563bc9cf949ccb3b8c2a8c
rbm0407 <rodrigo@rbm.dev.br>
2025-06-02 19:26:57 +0000

    add yamlls

---
+++ .config/nvim/lua/plugins/yaml.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "yamlls",
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      indent = {
+        enable = true,
+        disable = { "yaml" },
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        yamlls = {
+          settings = {
+            yaml = {
+              format = {
+                enable = true,
+              },
+              validate = true,
+              hover = true,
+              completion = true,
+              schemas = {
+                ["kubernetes"] = "0*.yml",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
