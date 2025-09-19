dde9496b5dc4f62995d689c6a4fc9d70211ed899
rbm0407 <rodrigo@rbm.dev.br>
2025-09-19 19:40:43 +0000

    fix blade treesitter on nvim 0.11 and lazyvim 15.x

--- .config/nvim/lua/plugins/php.lua
+++ .config/nvim/lua/plugins/php.lua
@@ -4,25 +4,16 @@ return {
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         "blade",
-        "php_only",
       })
-    end,
-    config = function(_, opts)
-      vim.filetype.add({
-        pattern = {
-          [".*%.blade%.php"] = "blade",
-        },
-      })
-
-      require("nvim-treesitter.configs").setup(opts)
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.blade = {
+      opts.parsers = opts.parsers or {}
+      opts.parsers.blade = {
         install_info = {
           url = "https://github.com/EmranMR/tree-sitter-blade",
           files = { "src/parser.c" },
           branch = "main",
         },
         filetype = "blade",
+        main_filetype = "php",
       }
     end,
   },
