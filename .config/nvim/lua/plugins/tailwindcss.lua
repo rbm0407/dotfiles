9de720571aac738384d7ca40a59cdf50cb9bcc25
rbm0407 <rodrigo@rbm.dev.br>
2026-01-06 20:38:15 +0000

    fix tailwind lsp on templ

--- .config/nvim/lua/plugins/tailwindcss.lua
+++ .config/nvim/lua/plugins/tailwindcss.lua
@@ -1,26 +1,21 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        tailwindcss = {
-          filetypes = {
-            "html",
-            "css",
-            "scss",
-            "javascript",
-            "typescript",
-            "templ",
+    opts = function(_, opts)
+      local lspconfig = require("lspconfig")
+
+      lspconfig.tailwindcss.setup({
+        cmd = { "tailwindcss-language-server", "--stdio" },
+        filetypes = { "templ", "html", "css", "scss", "javascript", "typescript" },
+        root_dir = function(fname)
+          return lspconfig.util.root_pattern("tailwind.config.js", "tailwind.config.ts", ".git")(fname)
+        end,
+        init_options = {
+          userLanguages = {
+            templ = "html",
           },
-          root_dir = require("lspconfig.util").root_pattern(
-            "tailwind.config.js",
-            "tailwind.config.ts",
-            "postcss.config.js",
-            "package.json",
-            ".git"
-          ),
         },
-      },
-    },
+      })
+    end,
   },
 }
