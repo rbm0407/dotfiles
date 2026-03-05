5748af8ac8789c79c4b5b608cec369a51ff47515
rbm0407 <rodrigo@rbm.dev.br>
2026-03-05 19:50:46 +0000

    add foot into basic packages, adjust qutebrowser to edit on foot term

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,5 +1,5 @@
 config.load_autoconfig()
-config.set('editor.command', ['ghostty', '-e', 'nvim', '{file}'])
+config.set('editor.command', ['foot', '-e', 'nvim', '{file}'])
 config.set('colors.webpage.preferred_color_scheme', 'dark')
 
 config.source('privacy.py')
